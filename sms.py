3c3f391700fe4ba2ce7060fe9a750425089d1340
Faizan Alam <51821426+Faizan-Alam-1@users.noreply.github.com>
2021-10-30 09:45:10 +0530

    Update sms.py

--- sms.py
+++ sms.py
@@ -2,7 +2,7 @@ from twilio.rest import Client
 import speak
 
 def sms(text) :
-    client = Client("ACc7d414df5a2772ddffae6e54f79f6756" , "39c30d224f05a2758f49480aa13305e3")
+    client = Client("" , "")
     client.messages.create(to = ["+918210410103"],
        from_ = "+13512072081",
        body = text)
